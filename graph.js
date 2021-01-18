4ec7a8e461210a7d977b7da415aa3a377ded46b8
Diogo Basso <diogojbasso@gmail.com>
2021-01-18 01:58:47 -0300

    fixed gitignore

--- graph.js
+++ graph.js
@@ -1,34 +1,50 @@
-airports = 'BRA GRU CGH AJU FLN JOI LDB XAP'.split(' ');
-routes = [['BRA', 'GRU'], ['BRA', 'AJU'], ['XAP', 'LDB'], ['FLN', 'AJU'], ['CGH', 'GRU'], ['JOI', 'AJU'], ['JOI', 'AJU'], ['CGH', 'GRU']];
-var adjencyList = new Map();
+airports = "BRA GRU CGH AJU FLN JOI LDB XAP".split(" ");
+routes = [
+  ["BRA", "GRU"],
+  ["BRA", "AJU"],
+  ["XAP", "LDB"],
+  ["FLN", "AJU"],
+  ["CGH", "GRU"],
+  ["JOI", "AJU"],
+  ["JOI", "AJU"],
+  ["CGH", "GRU"],
+];
+
+let adjencyList = new Map();
+
 function addNode(airport) {
-    adjencyList.set(airport, []);
+  adjencyList.set(airport, []);
 }
+
 function addEdge(origin, destination) {
-    adjencyList.get(origin).push(destination);
-    adjencyList.get(destination).push(origin);
+  adjencyList.get(origin).push(destination);
+  adjencyList.get(destination).push(origin);
 }
+
 airports.forEach(addNode);
-routes.forEach(function (route) { return addEdge.apply(void 0, route); });
+
+routes.forEach(function (route) {
+  return addEdge.apply(void 0, route);
+});
 console.log(adjencyList);
+
 function bfs(start) {
-    var visited = new Set();
-    var queue = [start];
-    while (queue.length > 0) {
-        var airport = queue.shift();
-        var destinations = adjencyList.get(airport);
-        for (var _i = 0, destinations_1 = destinations; _i < destinations_1.length; _i++) {
-            destination = destinations_1[_i];
-            queue.push(destination);
-            if (destination === 'AJU') {
-                console.log(destination);
-            }
-            if (!visited.has(destination)) {
-                visited.add(destination);
-                queue.push(destination);
-            }
-        }
+  var visited = new Set();
+  var queue = [start];
+  while (queue.length > 0) {
+    var airport = queue.shift();
+    var destinations = adjencyList.get(airport);
+    for (var i = 0; i < destinations.length; i++) {
+      destination = destinations[i];
+      queue.push(destination);
+      if (destination === "AJU") {
+        console.log(destination);
+      }
+      if (!visited.has(destination)) {
+        visited.add(destination);
+        queue.push(destination);
+      }
     }
+  }
 }
-bfs('JOI');
-//# sourceMappingURL=graph.js.map
+bfs("JOI");
--- readme.md
+++ readme.md
@@ -8,10 +8,12 @@ traquejo em JS.
 Assista aqui em inglês: [https://JavaScript30.com](https://JavaScript30.com)
 Assista em Português:
 
-## Benefícios do #JavaScript30
+## **Benefícios** do #JavaScript30
 
 1. Uma forma de engajar, por meio de desafios em aplicações web, e transformar o javascript, abordando a **aplicações do mundo real**;
 2. Uma **consistência** no aprendizado haja visto que é encorajado, durante 30 dias, o aprendizado de JS;
-3. Uma didática impressionante no que concerne à funções e simplificações dessa.
-
-   e muito mais benefícios que só serão possíveis caso entre no curso.
+3. Uma didática impressionante no que concerne à funções nativas do browser, o que culmina numa simplificação, acarretando em facilidade para implementação de projetos pessoais;
+4. Um estímulo muito grande para transformar o que aprendera num projeto pessoal, num repositório para que além de treinar, sane dores de usuários reais!
+   ...
+   <br>
+   E muito mais benefícios que só serão possíveis caso entre no curso.
